d373b212e51b1037fae261feb9933f12d1a64886
Daryl Norrell <cheyannewascom@gmail.com>
2022-07-27 14:31:44 -0700

    commit tree 09df0e93eec1d13c29771726b389ab4ed017a8e3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const body = document.querySelector("body");
+const main = body.querySelector("main");
+body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Daryl is the champion"
+document.body.append(newHeader);
